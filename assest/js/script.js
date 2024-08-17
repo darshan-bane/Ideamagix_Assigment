8923054c5185ca7e31cbc7140a997533e0b9c710
darshan-bane <157870188+darshan-bane@users.noreply.github.com>
2024-08-17 19:36:18 +0530

    Update script.js

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,14 +1,12 @@
-// Select elements
+// ============== hamburger menu ==================
 const hamburger = document.getElementById('hamburger');
 const sidebar = document.getElementById('sidebar');
 const closeBtn = document.getElementById('closeBtn');
 
-// Toggle sidebar on hamburger click
 hamburger.addEventListener('click', () => {
     sidebar.style.left = '0';
 });
 
-// Close sidebar on close button click
 closeBtn.addEventListener('click', () => {
     sidebar.style.left = '-250px';
 });
