5ffced6694241e0e8ce7d216390770a22bb72fe0
darshan-bane <darshubetaa@gmail.com>
2024-08-17 16:16:37 +0530

    done with faq

--- assest/js/script.js
+++ assest/js/script.js
@@ -15,11 +15,31 @@ closeBtn.addEventListener('click', () => {
 
 
 
+// ==================== Lazy load ======================
+document.addEventListener("DOMContentLoaded", function () {
+    const lazyImage = document.querySelector('.lazy-load');
 
+    const loadImage = () => {
+        if (lazyImage.getBoundingClientRect().top < window.innerHeight && lazyImage.getBoundingClientRect().bottom > 0) {
+            lazyImage.src = lazyImage.getAttribute('data-src');
+            lazyImage.classList.remove('lazy-load');
+            window.removeEventListener('scroll', loadImage);
+        }
+    };
 
+    window.addEventListener('scroll', loadImage);
+    loadImage(); // Check if image is already in view on page load
+});
 
+// ================= accordian ===============
+const acordians = document.querySelectorAll('.acordian')
 
+acordians.forEach(acordian => {
+    const icon = acordian.querySelector('.icon')
+    const answer = acordian.querySelector('.answer')
 
-
-
-
+    acordian.addEventListener('click', () => {
+        icon.classList.toggle('active')
+        answer.classList.toggle('active')
+    })
+})
